99fb8940dea47d8ab8a0e6f6090a27efa7472c8a
Matthew Sit <msit@berkeley.edu>
2017-08-02 12:14:14 -0700

    fix firefox zooming

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 
     /** Firefox uses different scaling. **/
     if (navigator.userAgent.indexOf("Firefox") > 0){
-        upperZoomLimit /= 100;
-        lowerZoomLimit /= 100;
-        stepSize /= 100;
+        upperZoomLimit /= 125;
+        lowerZoomLimit /= 85;
+        stepSize = 1.1;
     }
 
     /** On load, use previous settings. **/
@@ -18,7 +18,7 @@ $(document).ready(function() {
             window.name /= 100;
         }
     }
-    if (typeof readData === "function") { 
+    if (typeof readData === "function") {
         readData();
     }
     update();
@@ -71,22 +71,32 @@ $(document).ready(function() {
             $(".text-area").css('MozTransform','scale(' + window.name + ')');
             $("#page-title").css('MozTransform','scale(' + window.name + ')');
             $(".subtitle").css('MozTransform','scale(' + window.name + ')');
-            $("#footer").css('MozTransform','scale(' + window.name + ')');
-            $("#zoom-in").css('-moz-filter', 'grayscale(0%)');
+            $("#home-div").css('MozTransformOrigin','50% 0');
+            $(".text-area").css('MozTransformOrigin','50% 0');
+            $("#page-title").css('MozTransformOrigin','0 0');
+            $(".subtitle").css('MozTransformOrigin','0 0');
+            $("#alumni-subtitle").css('MozTransformOrigin','0 -3500%');
+            $("#alumni").css('MozTransformOrigin','0 -50%');
+            if (window.innerWidth < 960) {
+                $("#alumni-subtitle").css('MozTransformOrigin','0 -6500%');
+                $("#alumni").css('MozTransformOrigin','0 -100%');
+            }
+            $("#zoom-in").css('-webkit-filter', 'grayscale(0%)');
             $("#zoom-in").css('cursor', 'pointer');
-            $("#zoom-out").css('-moz-filter', 'grayscale(0%)');
+            $("#zoom-out").css('-webkit-filter', 'grayscale(0%)');
             $("#zoom-out").css('cursor', 'pointer');
 
             if (window.name >= upperZoomLimit) {
-                $("#zoom-in").css('-moz-filter', 'grayscale(100%)');
+                $("#zoom-in").css('-webkit-filter', 'grayscale(100%)');
                 $("#zoom-in").css('cursor', 'default');
             }
 
             if (window.name <= lowerZoomLimit) {
-                $("#zoom-out").css('-moz-filter', 'grayscale(100%)');
+                $("#zoom-out").css('-webkit-filter', 'grayscale(100%)');
                 $("#zoom-out").css('cursor', 'default');
             }
         }
     }
 
+    $(window).resize(update);
 });
