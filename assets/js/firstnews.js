250820e0ff2bcbea7bf4e3fadee4498c613885fc
hedch <dongcheng.he@du.edu>
2023-06-05 13:13:19 -0700

    Update firstnews.js

--- assets/js/firstnews.js
+++ assets/js/firstnews.js
@@ -1,25 +1,23 @@
-var data = null;
+var data = ' ';
 function doData(json) {
-   // if(json.feed.entry[0].title['$t']=='selab_news')
-   //     data = json.feed.entry[0].content;
-   // else
-    data = json.feed.entry[1].content;
-
+    if(json.feed.entry[0].title.$t=='selab_news')
+        data = json.feed.entry[0].content;
+    else
+        data = json.feed.entry[1].content;
+    console.log(data);
 }
 
 function readData() {
     
-    //data = data['$t'].split('<p><br />');
+    data = data['$t'].split('<p><br />');
     var htmlExpr = '';
 
-    //var dataLine = data[0].split('</p>');
+    var dataLine = data[0].split('</p>');
     htmlExpr += '<div class=\"text-area\">';
-    htmlExpr += '<div class=\"detail\">' + data['$t'] + '</div>';
-    //htmlExpr += '<div class=\"headline\">' + dataLine[1].substring(3) + '</div>';
-    //htmlExpr += '<div class=\"date\">' + dataLine[0].substring(3) + '</div>';
-    //htmlExpr += '<div class=\"detail\">' + dataLine[2].substring(3) + '</div>';
+    htmlExpr += '<div class=\"headline\">' + dataLine[1].substring(3) + '</div>';
+    htmlExpr += '<div class=\"date\">' + dataLine[0].substring(3) + '</div>';
+    htmlExpr += '<div class=\"detail\">' + dataLine[2].substring(3) + '</div>';
     htmlExpr += '</div>';
 
     document.getElementById("news-snippet").innerHTML += htmlExpr;
-    
 }
