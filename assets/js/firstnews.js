89776036a24b7e52996004fe93d5ebe69fc5533d
hedch <dongcheng.he@du.edu>
2023-06-05 12:02:34 -0700

    Update firstnews.js

--- assets/js/firstnews.js
+++ assets/js/firstnews.js
@@ -1,6 +1,6 @@
 var data = null;
 function doData(json) {
-    if(json.feed.entry[0].title.$t=='selab_news')
+    if(json.feed.entry[0].title['$t']=='selab_news')
         data = json.feed.entry[0].content;
     else
         data = json.feed.entry[1].content;
@@ -14,12 +14,12 @@ function readData() {
 
     //var dataLine = data[0].split('</p>');
     htmlExpr += '<div class=\"text-area\">';
-    htmlExpr += '<div class=\"detail\">' + 'test' + '</div>';
+    htmlExpr += '<div class=\"detail\">' + data['$t'] + '</div>';
     //htmlExpr += '<div class=\"headline\">' + dataLine[1].substring(3) + '</div>';
     //htmlExpr += '<div class=\"date\">' + dataLine[0].substring(3) + '</div>';
     //htmlExpr += '<div class=\"detail\">' + dataLine[2].substring(3) + '</div>';
     htmlExpr += '</div>';
 
-    //document.getElementById("news-snippet").innerHTML += htmlExpr;
-    document.getElementById("news-snippet").innerText = data;
+    document.getElementById("news-snippet").innerHTML += htmlExpr;
+    
 }
