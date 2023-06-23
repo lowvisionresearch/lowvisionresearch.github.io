b4363ff8eee367824239a43760b8964df168b00f
hedch <dongcheng.he@du.edu>
2023-06-23 10:50:21 -0700

    Update firstnews.js

--- assets/js/firstnews.js
+++ assets/js/firstnews.js
@@ -1,6 +1,5 @@
 var data = ' ';
 function doData(json) {
-    console.log(json);
     if(json.feed.entry[0].title.$t=='selab_news')
         data = json.feed.entry[0].content;
     else
@@ -9,15 +8,14 @@ function doData(json) {
 
 function readData() {
     
-    //data = String(data['$t']);
-    console.log(data);
+    data = data['$t'].split('<p><br />');
     var htmlExpr = '';
 
-    //var dataLine = String(data[0]).split('</p>');
+    var dataLine = String(data[0]).split('</p>');
     htmlExpr += '<div class=\"text-area\">';
-    //htmlExpr += '<div class=\"headline\">' + String(dataLine[1]).substring(3) + '</div>';
-    //htmlExpr += '<div class=\"date\">' + String(dataLine[0]).substring(3) + '</div>';
-    htmlExpr += '<div class=\"detail\">' + 'test2' + '</div>';
+    htmlExpr += '<div class=\"headline\">' + String(dataLine[1]).substring(3) + '</div>';
+    htmlExpr += '<div class=\"date\">' + String(dataLine[0]).substring(3) + '</div>';
+    htmlExpr += '<div class=\"detail\">' + String(dataLine[2]).substring(3) +  '</div>';
     htmlExpr += '</div>';
 
     document.getElementById("news-snippet").innerHTML += htmlExpr;
